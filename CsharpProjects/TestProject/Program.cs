4648373d9f6a846475edb834400399514978533b
sebastian lapt <smoreira11@hotmail.com>
2025-09-09 08:11:55 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -3,8 +3,20 @@
 //string result = string.Format("{0} {1}!", first, second);
 //Console.WriteLine(result);
 
-string first = "Hello";
-string second = "World";
-Console.WriteLine($"{first} {second}!");
-Console.WriteLine($"{second} {first}!");
-Console.WriteLine($"{first} {first} {first}!");
+//string first = "Hello";
+//string second = "World";
+//Console.WriteLine($"{first} {second}!");
+//Console.WriteLine($"{second} {first}!");
+//Console.WriteLine($"{first} {first} {first}!");
+
+//decimal price = 123.45m;
+//int discount = 50;
+//Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b5ec0a97c86f96b04a6a18db19f26a31757b26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb237b5c5d5e5d1329df672ac4403ec55fc64286")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
