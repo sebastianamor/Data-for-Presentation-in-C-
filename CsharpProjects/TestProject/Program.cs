cb237b5c5d5e5d1329df672ac4403ec55fc64286
sebastian lapt <smoreira11@hotmail.com>
2025-09-08 11:46:24 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,10 @@
-﻿string first = "Hello";
+﻿//string first = "Hello";
+//string second = "World";
+//string result = string.Format("{0} {1}!", first, second);
+//Console.WriteLine(result);
+
+string first = "Hello";
 string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-Console.WriteLine(result);
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c59e3d6c9097e2bfeba8f905208bc243ca130f24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b5ec0a97c86f96b04a6a18db19f26a31757b26")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
