132fefd29f2718ba0b7686685fd644bee3ff2e15
sebastian lapt <smoreira11@hotmail.com>
2025-09-10 08:10:43 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,7 +1,7 @@
-﻿//string first = "Hello";
-//string second = "World";
-//string result = string.Format("{0} {1}!", first, second);
-//Console.WriteLine(result);
+﻿string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
 
 //string first = "Hello";
 //string second = "World";
@@ -13,10 +13,22 @@
 //int discount = 50;
 //Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
-decimal price = 67.55m;
-decimal salePrice = 59.99m;
+//decimal price = 67.55m;
+//decimal salePrice = 59.99m;
 
-string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+//string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
 
-yourDiscount += $"A discount of {((price - salePrice)/price):P1}!"; //inserted
-Console.WriteLine(yourDiscount);
+//yourDiscount += $"A discount of {((price - salePrice)/price):P1}!"; //inserted
+//Console.WriteLine(yourDiscount);
+
+//int invoiceNumber = 1201;
+//decimal productShares = 25.4568m;
+//decimal subtotal = 2750.00m;
+//decimal taxPercentage = .15825m;
+//decimal total = 3185.19m;
+
+//Console.WriteLine($"Invoice Number: {invoiceNumber}");
+//Console.WriteLine($"   Shares: {productShares:N3} Product");
+//Console.WriteLine($"     Sub Total: {subtotal:C}");
+//Console.WriteLine($"           Tax: {taxPercentage:P2}");
+//Console.WriteLine($"     Total Billed: {total:C}");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4648373d9f6a846475edb834400399514978533b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4239924e0872bcd97e5729488ae4d8005eb8628c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
