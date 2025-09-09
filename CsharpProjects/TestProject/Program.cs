4239924e0872bcd97e5729488ae4d8005eb8628c
sebastian lapt <smoreira11@hotmail.com>
2025-09-09 12:59:39 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -18,5 +18,5 @@ decimal salePrice = 59.99m;
 
 string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
 
-yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+yourDiscount += $"A discount of {((price - salePrice)/price):P1}!"; //inserted
 Console.WriteLine(yourDiscount);
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb237b5c5d5e5d1329df672ac4403ec55fc64286")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4648373d9f6a846475edb834400399514978533b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
